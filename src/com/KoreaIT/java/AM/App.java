a036d32919c2d1b0a174997a7649713c663042b9
kingssik <hyunsikkang25@gmail.com>
2023-02-03 21:39:47 +0900

    기능 수행 역할 App에서 완전 배제 후 Controller로 위임

--- src/com/KoreaIT/java/AM/App.java
+++ src/com/KoreaIT/java/AM/App.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Scanner;
 
 import com.KoreaIT.java.AM.controller.ArticleController;
+import com.KoreaIT.java.AM.controller.Controller;
 import com.KoreaIT.java.AM.controller.MemberController;
 import com.KoreaIT.java.AM.dto.Article;
 import com.KoreaIT.java.AM.dto.Member;
@@ -41,21 +42,28 @@ public class App {
 				break;
 			}
 
-			if (cmd.equals("member join")) {
-				memberController.doJoin();
-			} else if (cmd.equals("article write")) {
-				articleController.doWrite();
-			} else if (cmd.startsWith("article list")) {
-				articleController.showList(cmd);
-			} else if (cmd.startsWith("article detail ")) {
-				articleController.showDetail(cmd);
-			} else if (cmd.startsWith("article modify ")) {
-				articleController.doModify(cmd);
-			} else if (cmd.startsWith("article delete ")) {
-				articleController.doDelete(cmd);
-			} else {
-				System.out.println("없는 명령어입니다");
+			String[] cmdBits = cmd.split(" "); // article detail
+
+			if (cmdBits.length == 1) {
+				System.out.println("존재하지 않는 명령어입니다");
+				continue;
 			}
+
+			String controllerName = cmdBits[0]; // article
+			String actionMehtodName = cmdBits[1]; // list
+
+			Controller controller = null;
+
+			if (controllerName.equals("article")) {
+				controller = articleController;
+			} else if (controllerName.equals("member")) {
+				controller = memberController;
+			} else {
+				System.out.println("존재하지 않는 명령어입니다");
+				continue;
+			}
+
+			controller.doAction(cmd, actionMehtodName);
 		}
 
 		sc.close();
