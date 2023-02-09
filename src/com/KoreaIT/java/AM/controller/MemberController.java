4545e879e6fad9a66b4f38785ffd8050e1974d00
kingssik <hyunsikkang25@gmail.com>
2023-02-09 19:35:21 +0900

    로그인 로그아웃 체크가드를 App에 추가, 각 컨트롤러 내에서 확인기능 삭제

--- src/com/KoreaIT/java/AM/controller/MemberController.java
+++ src/com/KoreaIT/java/AM/controller/MemberController.java
@@ -40,21 +40,11 @@ public class MemberController extends Controller {
 	}
 
 	private void doLogout() {
-		if (isLogined() == false) {
-			System.out.println("로그인 상태가 아닙니다");
-			return;
-		}
-
 		loginedMember = null;
 		System.out.println("로그아웃 되었습니다");
 	}
 
 	private void doLogin() {
-		if (isLogined()) {
-			System.out.println("로그아웃 후 이용하세요");
-			return;
-		}
-
 		System.out.printf("로그인 아이디 : ");
 		String loginId = sc.nextLine();
 		System.out.printf("로그인 비밀번호 : ");
